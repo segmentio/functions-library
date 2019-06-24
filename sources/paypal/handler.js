48c9a0eb9643f998c3f5fe6997abf97d98fb0bae
n2parko <n2parko@users.noreply.github.com>
2019-06-24 16:55:22 -0700

    Update handler.js

--- sources/paypal/handler.js
+++ sources/paypal/handler.js
@@ -6,7 +6,7 @@ exports.processEvents = async (event) => {
     objects: [],
     events: []
   }
-
+  
   if (eventBody.resource_type == 'order') {
     	const orderObj = {
           collection: eventBody.resource_type,
@@ -49,7 +49,6 @@ exports.processEvents = async (event) => {
   }
 
   if (eventBody.resource_type == 'checkout-order') {
-
      const props = {
             createTime: eventBody.resource.create_time,
             updateTime: eventBody.resource.update_time,
