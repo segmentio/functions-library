b44290a4711ac7f4a6372f058d576855f4da7bc7
Noah Zoschke <noah@segment.com>
2019-06-27 14:24:21 -0700

    remove debug

--- sources/github/handler.js
+++ sources/github/handler.js
@@ -1,14 +1,15 @@
 exports.processEvents = async (event) => {
-    const body = JSON.parse(event.payload.body.payload)
-    const { headers, queryParams } = event.payload
-    
-    let evt = `${headers['X-Github-Event']}`
-    if (body.action) {
-      evt += ` ${body.action}`
-    }
-   
-    return({
-      events: [{
+  const body = JSON.parse(event.payload.body.payload)
+  const { headers, queryParams } = event.payload
+
+  let evt = `${headers['X-Github-Event']}`
+  if (body.action) {
+    evt += ` ${body.action}`
+  }
+
+  return ({
+    events: [
+      {
         type: 'track',
         event: evt,
         userId: `${body.sender.id}`,
@@ -17,6 +18,7 @@ exports.processEvents = async (event) => {
           headers,
           queryParams,
         },
-      }]
-    })
-  }
+      }
+    ]
+  })
+}
