b1c1cde9fd11e36d07fa01709d26f528f864b86b
Noah Zoschke <noah@segment.com>
2019-06-27 10:09:35 -0700

    github wip

---
+++ sources/github/handler.js
@@ -0,0 +1,22 @@
+exports.processEvents = async (event) => {
+    const body = JSON.parse(event.payload.body.payload)
+    const { headers, queryParams } = event.payload
+    
+    let evt = `${headers['X-Github-Event']}`
+    if (body.action) {
+      evt += ` ${body.action}`
+    }
+   
+    return({
+      events: [{
+        type: 'track',
+        event: evt,
+        userId: `${body.sender.id}`,
+        properties: {
+          body,
+          headers,
+          queryParams,
+        },
+      }]
+    })
+  }
