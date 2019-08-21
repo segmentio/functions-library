864700b0d586d1db798a8af802bc162ab8c62bcb
Chris Sperandio <chris@sperand.io>
2019-08-21 13:15:19 -0700

    Add comments / improve readability

--- destinations/datadog/index.js
+++ destinations/datadog/index.js
@@ -1,4 +1,19 @@
 
+// CONFIG - defaults are probably best, but feel free to update
+// 
+// nb.  all of these "config" inputs could be updated to be "parameterized"
+// so that you could dynamically set them in the UI (like apiKey)
+// via per-source via settings values in the future
+
+// if you dont want one of these tags below, you can just comment them out
+// EVENT_TYPE_TAG is probably riskiest — if you have a ton of event names / dynamic events
+// you could go over the cardinality limits
+const SOURCE_ID_TAG = 'source'
+const MESSAGE_TYPE_TAG = 'type'
+const EVENT_TYPE_TAG = 'event'
+
+const METRIC_NAME = 'segment.messages'
+
 async function track(msg, { apiKey }) {
   return sendMetric(msg, apiKey)
 }
@@ -24,23 +39,27 @@ async function screen(msg, { apiKey }) {
 }
   
 async function sendMetric(msg, apiKey) {
-  const series = [{
-    metric: 'segment.messages',
+  const metric = {
+    metric: METRIC_NAME,
     points: [[Math.round(Date.now() / 1000), 1]],
     type: 'count',
-    tags: [`source:${msg.projectId}`,`type:${msg.type}`]
-  }]
+    tags: []
+  }
   
-  if (msg.type === 'track') series[0].tags.push(`event:${msg.event}`)
+  if (EVENT_TYPE_TAG && msg.type === 'track') 
+    metric.tags.push(`${EVENT_TYPE_TAG}:${msg.event}`)
+  if (MESSAGE_TYPE_TAG)
+    metric.tags.push(`${MESSAGE_TYPE_TAG}:${msg.type}`)
+  if (SOURCE_ID_TAG) 
+    metric.tags.push(`${SOURCE_ID_TAG}:${msg.projectId}`)
   
-  const req = {
-    body: JSON.stringify({ series }),
+  const res = await fetch(`https://api.datadoghq.com/api/v1/series?api_key=${apiKey}`, {
+    body: JSON.stringify({ series: [ metric ] }),
     headers: new Headers({
       "Content-Type": "application/json",
     }),
     method: "post"
-  }
-
-  const res = await fetch(`https://api.datadoghq.com/api/v1/series?api_key=${apiKey}`, req)
+  })
+  
   return await res.json()
 }
