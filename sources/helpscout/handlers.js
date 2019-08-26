af3442a6f33b166cf4e1692a9e84ea548e60f8a4
Ben Horowitz <ben.horowitz@segment.com>
2019-08-26 15:25:29 -0700

    Implement HelpScout example

--- sources/helpscout/handlers.js
+++ sources/helpscout/handlers.js
@@ -1,44 +1,46 @@
 // 1. Access your event body, headers and query parameters through the event object
 // 2. Transform the event into Segment Tracking Events or Objects by returning an object with the appropriate keys
 
+//NOT FOR PRODUCTION USE
+
+//TODO: verify Helpscout signature
+
 exports.processEvents = async (event) => {
   let eventBody = event.payload.body;
   let eventHeaders = event.payload.headers;
   let queryParameters = event.payload.queryParameters;
+  
+  console.log(eventHeaders['X-Helpscout-Event'][0]);
+  
+  let eventType = eventHeaders['X-Helpscout-Event'][0].split('.')[0]
+  
+  console.log(eventType);
+  
+  let type = ''
 
-  // Return an object with any combination of the following keys
+  if (eventType == 'convo') {
+    type = 'conversation'
+  } else if (eventType == 'customer') {
+    type = 'customer'
+  } else if (eventType == 'satisfaction') {
+    type = 'rating'
+  } else {
+    callback(new Error('No matching event type (must be one of Conversation, Customer, Rating)'))
+  }
+  
+  console.log(type);
+
   let returnValue = {
-    events: [{
-        type: 'identify',
-        userId: '1234',
-        properties: {
-          propertyName: 'Example Property'
-        }
-    },
-    {
-        type: 'track',
-        event: 'Event Name',
-        userId: '1234',
-        properties: {
-          propertyName: 'Example Property'
-        }
-    },
-    {
-        type: 'group',
-        userId: '1234',
-        groupId: '1234',
-        properties: {
-          propertyName: 'Example Property'
-        }
-    }],
     objects: [{
-      collection: 'Collection Name - Plural',
-      id: "String of Object ID",
+      collection: type,
+      id: eventBody.id.toString(),
       properties: {
-        propertyName: 'Example Property'
+        type: eventBody.type
       }
     }]
   }
+  
+  console.log(returnValue);
 
   // Return the Javascript object with a key of events, objects or both
   return(returnValue)
