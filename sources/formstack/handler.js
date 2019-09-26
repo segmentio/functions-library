646997118060f0a7ffad444ab961fbff2be4cf4d
Noah Zoschke <noah@segment.com>
2019-09-26 14:08:17 -0700

    fix formstack

--- sources/formstack/handler.js
+++ sources/formstack/handler.js
@@ -1,5 +1,11 @@
-exports.processEvents = async (event) => {
-  const msg = event.payload.body
+/**
+* Please do not delete [used for Intellisense]
+* @param {ServerRequest} request The incoming webhook request
+* @param {Object.<string, any>} settings Custom settings
+* @return void
+*/
+async function onRequest(request, settings) {
+  const msg = request.json()
   const { first, last } = msg.Name.value
   const { email } = msg.Email.value
   const userId = msg.UniqueID
@@ -7,30 +13,20 @@ exports.processEvents = async (event) => {
   const optin = msg.optin.value ? true : false
   
   // create Leads in SFDC or user profiles downstream
-  const identify = {
-      type: 'identify',
-      userId,
-      traits: {
-        firstName: first,
-        lastName: last,
-        email,
-        optin
-      }
+  Segment.identify({
+    userId,
+    traits: {
+      firstName: first,
+      lastName: last,
+      email,
+      optin
     }
+  })
   
   // send explicit track event for funnel analyses and audiencing downstream
-  const track = {
-    type: 'track',
+  Segment.track({
     event: 'Form Submitted',
     userId,
     properties: { formId, email }
-  }
-
-  // Return an object with any combination of the following keys
-  let returnValue = {
-    events: [identify, track]
-  }
-
-  // Return the Javascript object with a key of events, objects or both
-  return(returnValue)
+  })
 }
