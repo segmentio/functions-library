998869c93064c1b5f02f38d6208feb88b6b9a6fc
Noah Zoschke <noah@segment.com>
2019-09-26 14:22:00 -0700

    fix google sheets

--- sources/google-sheets/handler.js
+++ sources/google-sheets/handler.js
@@ -1,36 +1,31 @@
-exports.processEvents = async event => {
-  let eventBody = event.payload.body;
-  let eventHeaders = event.payload.headers;
-  let queryParameters = event.payload.queryParameters;
+/**
+* Please do not delete [used for Intellisense]
+* @param {ServerRequest} request The incoming webhook request
+* @param {Object.<string, any>} settings Custom settings
+* @return void
+*/
+async function onRequest(request, settings) {
+  let eventBody = request.json();
 
-  // Return an object with any combination of the following keys
-  let returnValue = {
-    events: [
-      {
-        type: "identify",
-        userId: eventBody.userId,
-        traits: {
-          status: eventBody.status
-        },
-        context: {
-          source: "google sheets"
-        }
-      },
-      {
-        type: "track",
-        event: "Row Updated",
-        userId: eventBody.userId,
-        properties: {
-          email: eventBody.email,
-          status: eventBody.status
-        },
-        context: {
-          source: "google sheets"
-        }
-      }
-    ]
-  };
+  Segment.identify({
+    userId: eventBody.userId,
+    traits: {
+      status: eventBody.status
+    },
+    context: {
+      source: "google sheets"
+    }
+  })
 
-  // Return the Javascript object with a key of events, objects or both
-  return returnValue;
-};
+  Segment.track({
+    event: "Row Updated",
+    userId: eventBody.userId,
+    properties: {
+      email: eventBody.email,
+      status: eventBody.status
+    },
+    context: {
+      source: "google sheets"
+    }
+  })
+}
