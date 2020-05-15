147edea3cf4f346e8bec1271fc9cc4e695fe9610
Reid Sata <reid.t.sata@gmail.com>
2020-05-15 15:35:06 -0700

    twilio destination

---
+++ destinations/twilio/handler.js
@@ -0,0 +1,47 @@
+async function onIdentify(event, settings) {
+	try {
+		const auth = `${settings.accountSid}:${settings.authToken}`;
+
+		// check for phone number
+		if (!event.traits.phone) throw new Error('No phone number');
+
+		// do a lookup to validate phone number
+		const rawNumber = encodeURI(event.traits.phone);
+		const lookup = await fetch(
+			`https://lookups.twilio.com/v1/PhoneNumbers/${rawNumber}`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: `Basic ${btoa(auth)}`
+				}
+			}
+		);
+		const lookupRes = await lookup.json();
+		// check for valid phone number
+		if (lookupRes.status === 404) throw new Error('Invalid phone number');
+
+		// set variables for message
+		const to = lookupRes.phone_number;
+		const message = settings.message;
+		const from = settings.from;
+		const body = encodeURI(`Body=${message}&To=${to}&From=${from}`);
+		let url = new URL(
+			`https://api.twilio.com/2010-04-01/Accounts/${settings.accountSid}/Messages.json`
+		);
+
+		// send the message
+		const send = await fetch(url.toString(), {
+			method: 'POST',
+			headers: {
+				Authorization: `Basic ${btoa(auth)}`,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body
+		});
+		const sendRes = await send.json();
+		// check for successful queue
+		if (sendRes.status !== 'queued') throw new Error(JSON.stringify(sendRes));
+	} catch (err) {
+		throw new Error(err);
+	}
+}
---
+++ destinations/twilio/readme.md
@@ -0,0 +1,7 @@
+## Twilio Destination
+This function sends text messages on identify calls. 
+
+## Notes
+* The event must contain `traits.phone`. 
+* This will not work with Personas as is. In order to add Personas support, you will need to lookup the phone number before checking for the `traits.phone` field.
+* This function supports any phone number format that successfully returns from [Twilio's lookup service](https://www.twilio.com/docs/lookup).
