1f953d53c53c37cf30d44ae3657a3f2726c8a0d2
Ju Hae Lee <juhaelee.usc@gmail.com>
2020-05-15 14:39:07 -0700

    unsubscribe

--- sources/convertkit/handler.js
+++ sources/convertkit/handler.js
@@ -11,17 +11,29 @@ async function onRequest(request, settings) {
   const body = request.json();
 
   // check if there's a subscriber as part of the payload
-  if (body && body.subscriber && body.subscriber.id) {
-    // See https://segment.com/docs/connections/spec/identify/
-    const subscriber = body.subscriber;
-    Segment.identify({
-      userId: subscriber.email_address,
-      traits: {
-        email: subscriber.email_address,
-        convertkit_id: subscriber.id,
-        converkit_status: subscriber.state,
-        firstName: subscriber.first_name,
-      },
-    });
+  const subscriber = body && body.subscriber;
+  if (!!subscriber && subscriber.id) {
+    switch (subscriber.state) {
+      case "cancelled":
+        Segment.track({
+          event: "Unsubscribed",
+          userId: subscriber.email_address,
+          properties: {},
+        });
+        break;
+      case "active":
+        Segment.identify({
+          userId: subscriber.email_address,
+          traits: {
+            email: subscriber.email_address,
+            convertkit_id: subscriber.id,
+            converkit_status: subscriber.state,
+            firstName: subscriber.first_name,
+          },
+        });
+        break;
+      default:
+        return;
+    }
   }
 }
